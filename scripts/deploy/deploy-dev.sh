0888f5580d4df65108e7bdbcc246f2d01d91fe3f
therewasaguy <jason.sigal@gmail.com>
2015-12-15 00:37:43 -0500

    update deploy script

--- scripts/deploy/deploy-dev.sh
+++ scripts/deploy/deploy-dev.sh
@@ -1,6 +1,6 @@
 #ssh into amazon s3 instance
 # OLD: ssh -i ~/.ssh/transients-keypair.pem ec2-user@52.11.202.99 << EOF
-ssh -i ~/.ssh/transients-keypair.pem ec2-user@ec2-52-11-57-55.us-west-2.compute.amazonaws.com << EOF
+ssh -i ~/.ssh/transients_key_pair.pem ec2-user@ec2-52-11-57-55.us-west-2.compute.amazonaws.com << EOF
 
 cd transients/dev/transients-server/ 
 
@@ -9,6 +9,8 @@ git fetch --all
 git checkout -b development origin/development
 
 git pull 
+
+cd /home/ec2-user/supervisor
 
 supervisorctl restart transients
 
